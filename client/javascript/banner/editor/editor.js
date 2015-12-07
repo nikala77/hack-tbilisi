70cbbb1048b1ae899035b4f3eb30bc4ea64ee094
radamel9 <ggola93@gmail.com>
2015-12-07 18:50:30 +0400

    sidebar initialized

--- client/javascript/banner/editor/editor.js
+++ client/javascript/banner/editor/editor.js
@@ -1,1 +1,28 @@
 console.log('editor');
+
+$(function() {
+  var backup_params;
+  var options = {
+    onMouseDown: function(){
+      var $this = $(this);
+      // transition-duration
+      backup_params = {
+        'transition-duration': $this.css('transition-duration')
+      };
+      var params = {
+        'transition-duration': '0'
+      };
+      $this.css(params);
+    }
+    ,onMouseUp: function(){
+      var $this = $(this);
+      $this.css(backup_params);
+    }
+  };
+  //
+  $('.cabinet-left').cabinet($.extend({
+    width: 300
+    ,mode: 'position'
+  }, options));
+  //
+});
